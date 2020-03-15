f550e5da8d0220830e9425281836dd39e5464842
piecego <wktrf@foxmail.com>
2020-03-15 18:50:58 +0800

    Update

--- src/helpers/cloud.ts
+++ src/helpers/cloud.ts
@@ -3,8 +3,8 @@ import db from 'leancloud-storage'
 if (typeof window !== 'undefined') {
   if (!db.applicationId || !db.applicationKey) {
     db.init({
-      appId: '',
-      appKey: '',
+      appId: '0JOLAdmlVLvjfdW8RL4klhkQ-gzGzoHsz',
+      appKey: 'lLC1D5xhP2l8MGdtoTRPwSnj',
       serverURLs: 'https://0joladml.lc-cn-n1-shared.com'
     })
   }
